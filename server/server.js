af6680caf6ea0f1ea6bc6b02f14dd63f42822442
LeandroKlaus <leklaussl@gmail.com>
2025-01-20 22:21:16 -0400

    Teste para lançar backend no ar

--- server/server.js
+++ server/server.js
@@ -8,13 +8,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Conecte-se ao MongoDB
-mongoose.connect('mongodb://localhost:27017/motodb', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// Defina o modelo de moto
 const motoSchema = new mongoose.Schema({
   modelo: String,
   imagem: String,
@@ -22,25 +20,22 @@ const motoSchema = new mongoose.Schema({
     descricao: String,
     valor: String
   },
-  fichaTecnica: String // Novo campo para a ficha técnica
+  fichaTecnica: String
 });
 
 const Moto = mongoose.model('Moto', motoSchema);
 
-// Rota para obter todas as motos
 app.get('/motos', async (req, res) => {
   const motos = await Moto.find();
   res.json(motos);
 });
 
-// Rota para adicionar uma nova moto
 app.post('/motos', async (req, res) => {
   const novaMoto = new Moto(req.body);
   await novaMoto.save();
   res.json(novaMoto);
 });
 
-// Rota para excluir uma moto
 app.delete('/motos/:id', async (req, res) => {
   await Moto.findByIdAndDelete(req.params.id);
   res.json({ message: 'Moto excluída' });
